a8df960ebdf66358ef937458ad3113ee3ab8fc44
Johannes Groedem <jog@niva.no>
2019-08-21 20:38:56 +0200

    Do Flask tracing with a simple WSGI middleware.

--- nivacloud_logging/flask_trace.py
+++ nivacloud_logging/flask_trace.py
@@ -1,24 +1,23 @@
-import functools
-import logging
-
-import flask
-
 from nivacloud_logging.log_utils import LogContext
 
 
-def trace(func):
+class TracingMiddleware:
     """
-    A decorator for Flask handlers that takes the Trace-Id header from
-    incoming HTTP headers and sets the "trace_id" LogContext to the value
-    of this header.
+    WSGI middleware that looks for a Trace-Id header in every request and adds it to
+    LogContext for that request if found.
+
+    Usage:
+      app = Flask(__name__)
+      app.wsgi_app = TracingMiddleware(app.wsgi_app)
     """
 
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        trace_ctx = {'trace_id': flask.request.headers.get('Trace-Id')}
-        with LogContext(**trace_ctx):
-            # TODO: Don't do this. Figure out how to inject it into the access log instead...
-            logging.info('Traced', extra=trace_ctx)
-            return func(*args, **kwargs)
+    def __init__(self, app):
+        self.app = app
 
-    return wrapper
+    def __call__(self, environ, start_response):
+        trace_id = environ.get('HTTP_TRACE_ID')
+        if trace_id:
+            with LogContext(trace_id=trace_id):
+                return self.app(environ, start_response)
+        else:
+            return self.app(environ, start_response)
--- tests/test_tracing.py
+++ tests/test_tracing.py
@@ -1,10 +1,11 @@
 import json
+import logging
 import re
 
 import requests
 from flask import Flask, jsonify
 
-from nivacloud_logging.flask_trace import trace
+from nivacloud_logging.flask_trace import TracingMiddleware
 from nivacloud_logging.log_utils import setup_logging, LogContext
 from nivacloud_logging.requests_trace import TracingAdapter
 
@@ -16,7 +17,7 @@ def _create_tracing_requests_session():
     return r
 
 
-def _is_tracelike(s):
+def _is_trace_like(s):
     return re.match('[a-f0-9]+', s)
 
 
@@ -27,7 +28,7 @@ def test_generate_trace_id_if_missing():
     headers = result.json()['headers']
 
     assert 'Trace-Id' in headers
-    assert _is_tracelike(headers.get('Trace-Id'))
+    assert _is_trace_like(headers.get('Trace-Id'))
 
 
 def test_trace_id_is_picked_up_from_context():
@@ -41,20 +42,19 @@ def test_trace_id_is_picked_up_from_context():
 
 def test_trace_id_is_injected(capsys):
     app = Flask(__name__)
+    app.wsgi_app = TracingMiddleware(app.wsgi_app)
+
     setup_logging()
 
     @app.route("/")
-    @trace
     def hello():
+        logging.info("HEI!")
         return jsonify({"Trace-ID": LogContext.getcontext("trace_id")})
 
     client = app.test_client()
-
     r = client.get("/", headers={'Trace-ID': '123abc'}).json
-
     assert r.get('Trace-ID') == "123abc"
 
     (out, _) = capsys.readouterr()
-    json_log = json.loads(out)
-
-    assert json_log['trace_id'] == '123abc'
+    parsed_output = json.loads(out)
+    assert parsed_output['trace_id'] == '123abc'
