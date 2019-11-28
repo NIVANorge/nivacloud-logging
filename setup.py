072d83e300a2e0297ac962115674806c9a93b03e
RmStorm <roaldstorm@gmail.com>
2019-11-28 14:14:22 +0100

    Another try to get the tag into circleci

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION = "0.8.10"
+VERSION = "0.8.11"
 
 OPTIONAL_REQUIREMENTS = {
     # These are optional dependencies needed for tracing:
