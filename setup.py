18268986f7fd17b78c703173f07cfafe1b44233f
RmStorm <roaldstorm@gmail.com>
2019-11-28 13:55:54 +0100

    Start running tests in 3.8.0 and try to publish on pypi
    I force pushed on the previous commit which seems to not have worked..

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 from setuptools import setup
 from setuptools.command.install import install
 
-VERSION = "0.8.8"
+VERSION = "0.8.9"
 
 OPTIONAL_REQUIREMENTS = {
     # These are optional dependencies needed for tracing:
