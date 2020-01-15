f28eb6f255eea14e04ceae388559e89285a812b8
Håkon Drolsum Røkenes <hakon.rokenes@niva.no>
2020-01-15 12:18:20 +0100

    Removed usage of walrus operator to be python 3.6 compatible

--- nivacloud_logging/aiohttp_trace.py
+++ nivacloud_logging/aiohttp_trace.py
@@ -10,9 +10,11 @@ def create_client_trace_config():
 
     async def on_request_start(session, trace_config_ctx, params):
         params.headers['Span-Id'] = LogContext.getcontext("span_id") or generate_trace_id()
-        if trace_id := LogContext.getcontext("trace_id"):
+        trace_id = LogContext.getcontext("trace_id")
+        if trace_id:
             params.headers['Trace-Id'] = trace_id
-        if user_id := LogContext.getcontext("user_id"):
+        user_id = LogContext.getcontext("user_id")
+        if user_id:
             params.headers['User-Id'] = user_id
 
 
